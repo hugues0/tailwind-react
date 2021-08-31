6251410ef0cc6be1c1398365702570703a33ee32
hugues0 <ntwari.hugues@gmail.com>
2021-08-31 14:14:02 +0200

    ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ContactCard from './ContactCard';
 
 function App() {
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 p-4">
       <section>
         <form>
-          <input placeholder="type here to fill" type="text" />
+          <input placeholder="type here to fill..." type="text" className="ml-20 mt-6 rounded-md p-2 m-10" />
         </form>
       </section>
-      <section>
+      <section className="p-20 grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
         <ContactCard />
       </section>
     </div>
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,9 +1,155 @@
 const ContactCard = () => {
     return (
-        <div>
-            <h1>Welcome to this app</h1>
-        </div>
-    )
+      <>
+        <figure className="bg-white h-80 rounded-lg shadow-md pt-7 ">
+          <img
+            className="w-32 h-32 rounded-full mx-auto"
+            alt="user"
+            src="https://cchubnigeria.com/designlab/wp-content/uploads/2020/04/DSC_0020-395x400.jpg"
+          />
+          <figcaption className="text-center mt-5">
+            <p className="text-gray-700 font-semibold text-xl m-2">
+              Hugues Ntwari
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">email:</span>ntwari.hugues@gmail.com
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">phone:</span> +250 781407229
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">city:</span> Kigali
+            </p>
+          </figcaption>
+        </figure>
+        <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+          <img
+            className="w-32 h-32 rounded-full mx-auto"
+            alt="user"
+            src="https://cchubnigeria.com/designlab/wp-content/uploads/2020/04/DSC_0020-395x400.jpg"
+          />
+          <figcaption className="text-center mt-5">
+            <p className="text-gray-700 font-semibold text-xl m-2">
+              Hugues Ntwari
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">email:</span>ntwari.hugues@gmail.com
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">phone:</span> +250 781407229
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">city:</span> Kigali
+            </p>
+          </figcaption>
+        </figure>
+        <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+          <img
+            className="w-32 h-32 rounded-full mx-auto"
+            alt="user"
+            src="https://cchubnigeria.com/designlab/wp-content/uploads/2020/04/DSC_0020-395x400.jpg"
+          />
+          <figcaption className="text-center mt-5">
+            <p className="text-gray-700 font-semibold text-xl m-2">
+              Hugues Ntwari
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">email:</span>ntwari.hugues@gmail.com
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">phone:</span> +250 781407229
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">city:</span> Kigali
+            </p>
+          </figcaption>
+        </figure>
+        <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+          <img
+            className="w-32 h-32 rounded-full mx-auto"
+            alt="user"
+            src="https://cchubnigeria.com/designlab/wp-content/uploads/2020/04/DSC_0020-395x400.jpg"
+          />
+          <figcaption className="text-center mt-5">
+            <p className="text-gray-700 font-semibold text-xl m-2">
+              Hugues Ntwari
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">email:</span>ntwari.hugues@gmail.com
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">phone:</span> +250 781407229
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">city:</span> Kigali
+            </p>
+          </figcaption>
+        </figure>
+        <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+          <img
+            className="w-32 h-32 rounded-full mx-auto"
+            alt="user"
+            src="https://cchubnigeria.com/designlab/wp-content/uploads/2020/04/DSC_0020-395x400.jpg"
+          />
+          <figcaption className="text-center mt-5">
+            <p className="text-gray-700 font-semibold text-xl m-2">
+              Hugues Ntwari
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">email:</span>ntwari.hugues@gmail.com
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">phone:</span> +250 781407229
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">city:</span> Kigali
+            </p>
+          </figcaption>
+        </figure>
+        <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+          <img
+            className="w-32 h-32 rounded-full mx-auto"
+            alt="user"
+            src="https://cchubnigeria.com/designlab/wp-content/uploads/2020/04/DSC_0020-395x400.jpg"
+          />
+          <figcaption className="text-center mt-5">
+            <p className="text-gray-700 font-semibold text-xl m-2">
+              Hugues Ntwari
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">email:</span>ntwari.hugues@gmail.com
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">phone:</span> +250 781407229
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">city:</span> Kigali
+            </p>
+          </figcaption>
+        </figure>
+        <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
+          <img
+            className="w-32 h-32 rounded-full mx-auto"
+            alt="user"
+            src="https://cchubnigeria.com/designlab/wp-content/uploads/2020/04/DSC_0020-395x400.jpg"
+          />
+          <figcaption className="text-center mt-5">
+            <p className="text-gray-700 font-semibold text-xl m-2">
+              Hugues Ntwari
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">email:</span>ntwari.hugues@gmail.com
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">phone:</span> +250 781407229
+            </p>
+            <p className="text-gray-500 ">
+              <span className="font-medium">city:</span> Kigali
+            </p>
+          </figcaption>
+        </figure>
+      </>
+    );
 }
 
 export default ContactCard

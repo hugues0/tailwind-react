ef3db1e40813987c2fe5e965acbc3c470b651e87
hugues0 <ntwari.hugues@gmail.com>
2021-08-31 12:08:24 +0200

    initial commit & setting up project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import ContactCard from './ContactCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100">
+      <section>
+        <form>
+          <input placeholder="type here to fill" type="text" />
+        </form>
+      </section>
+      <section>
+        <ContactCard />
+      </section>
     </div>
   );
 }
---
+++ src/ContactCard.js
@@ -0,0 +1,9 @@
+const ContactCard = () => {
+    return (
+        <div>
+            <h1>Welcome to this app</h1>
+        </div>
+    )
+}
+
+export default ContactCard
